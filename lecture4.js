9d132a62b5cd70827fb477dd4b435a5bff58706d
Owen <stefanescuowen@gmail.com>
2025-10-07 08:30:30 -0400

    First commit.

---
+++ lecture4.js
@@ -0,0 +1,19 @@
+const prompt = require ('prompt-sync');
+
+//Map
+let fruits = new Map();
+console.log(fruits);
+
+
+fruits.set("Apple", 400);
+fruits.set("Apple", 600);
+fruits.set("Mango", 500);
+fruits.set("Kiwi", 200);
+console.log(fruits);
+// console.log(fruits.get("Apple"));
+// console.log(fruits.has("Apple"));
+// console.log(fruits.size);
+// console.log(fruits.delete("Apple"));
+// console.log(fruits);
+// fruits.clear();
+// console.log(fruits);
