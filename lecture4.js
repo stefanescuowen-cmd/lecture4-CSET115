e5eba8fe3d315d2115094d657ed471bdf3349e57
Owen <stefanescuowen@gmail.com>
2025-10-07 08:50:29 -0400

    Started working on part 1.

--- lecture4.js
+++ lecture4.js
@@ -1,19 +1,39 @@
-const prompt = require ('prompt-sync');
-
-//Map
-let fruits = new Map();
-console.log(fruits);
+const prompt = require('prompt-sync')();
 
 
-fruits.set("Apple", 400);
-fruits.set("Apple", 600);
-fruits.set("Mango", 500);
-fruits.set("Kiwi", 200);
-console.log(fruits);
-// console.log(fruits.get("Apple"));
-// console.log(fruits.has("Apple"));
-// console.log(fruits.size);
-// console.log(fruits.delete("Apple"));
-// console.log(fruits);
-// fruits.clear();
-// console.log(fruits);
+// Map Example:
+
+    // let fruits = new Map();
+    // console.log(fruits);
+
+
+    // fruits.set("Apple", 400);
+    // fruits.set("Apple", 600);
+    // fruits.set("Mango", 500);
+    // fruits.set("Kiwi", 200);
+    // console.log(fruits);
+    // console.log(fruits.get("Apple"));
+    // console.log(fruits.has("Apple"));
+    // console.log(fruits.size);
+    // console.log(fruits.delete("Apple"));
+    // console.log(fruits);
+    // fruits.clear();
+    // console.log(fruits);
+
+// #1
+
+function removeEven(arr){
+    return arr.filter(num => num % 2 !== 0)
+}
+
+// #2
+
+
+
+// #3
+
+
+
+// #4
+
+
