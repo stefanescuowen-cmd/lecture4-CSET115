1c234f5b464761ab532cad7510652686ed32ac74
Owen <stefanescuowen@gmail.com>
2025-10-07 09:21:00 -0400

    Finished parts 3 and 4.

--- lecture4.js
+++ lecture4.js
@@ -33,15 +33,30 @@ const prompt = require('prompt-sync')();
 
 // #2
 
-const arr = [1, 2, 2, 3, 4, 4, 5];
-const uniqueSet = new Set(arr);
-const uniqueArray = [...uniqueSet];
-console.log(uniqueArray);
+// const arr = [1, 2, 2, 3, 4, 4, 5];
+// const uniqueSet = new Set(arr);
+// const uniqueArray = [...uniqueSet];
+// console.log(uniqueArray);
 
 // #3
 
+function check(arr, num){
+    return arr.includes(num);
+}
+
+console.log(check([1, 2, 3, 4, 5], 3))
+console.log(check([1, 1, 2, 1, 1], 3))
+console.log(check([5, 5, 5, 6], 5))
+console.log(check([], 5))
 
 
 // #4
 
+function numObj(arr){
+    return arr.map(num =>{
+        return {[num]: String.fromCharCode(num)};
+    });
+}
 
+console.log(numObj([118, 117, 120]));
+console.log([101, 121, 110, 113, 103])
